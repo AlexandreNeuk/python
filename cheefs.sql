56d28b815fdd8dd02bf5b3834333f6c0ee6e0c67
Alexandre Neukirchen <aneukirchen@hotmail.com>
2020-08-19 22:09:05 -0300

    Update cheefs.sql

--- cheefs.sql
+++ cheefs.sql
@@ -5,14 +5,15 @@ from clientes
 group by sexo
 
 -- Questão 2
-SELECT  '18-20' Age_Range, COUNT(CASE WHEN idade BETWEEN 18 AND 20 THEN idade ELSE NULL END) Qtd
-FROM clientes
+(SELECT  '18-20' Age_Range, COUNT(CASE WHEN idade BETWEEN 18 AND 20 THEN idade ELSE NULL END) Qtd
+FROM clientes)
 Union
-SELECT  '21-23', COUNT(CASE WHEN idade BETWEEN 21 AND 23 THEN idade ELSE NULL END) Qtd
-FROM clientes
+(SELECT  '21-23', COUNT(CASE WHEN idade BETWEEN 21 AND 23 THEN idade ELSE NULL END) Qtd
+FROM clientes)
 Union
-SELECT  '24-27', COUNT(CASE WHEN idade BETWEEN 24 AND 27 THEN idade ELSE NULL END) Qtd
-FROM clientes
+(SELECT  '24-27', COUNT(CASE WHEN idade BETWEEN 24 AND 27 THEN idade ELSE NULL END) Qtd
+FROM clientes)
+order by age_range
 
 -- Questão 3
 select count(*) as "Total", c.plano as "Tipo Plano"
